961f874f6577ea54f02d5e3f743fd793c80d412e
light011-glitch <69357237+light011-glitch@users.noreply.github.com>
2021-05-27 11:05:29 +0530

    Create Sales_by_match.py

---
+++ Sales_by_match.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    a = 0 
+    b = set()
+    for i in range(n):
+        if ar[i] not in b:
+            b.add(ar[i])
+        else:
+            a+=1
+            b.remove(ar[i])
+    return a
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
