cbb34f3f3a6c0c5d4d789792e0c7f24453037a64
light011-glitch <69357237+light011-glitch@users.noreply.github.com>
2021-09-18 11:47:42 +0530

    Create Mars_exploration.py

---
+++ Mars_exploration.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marsExploration' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts STRING s as parameter.
+#
+
+def marsExploration(s):
+    # Write your code here
+    S = s.strip()
+    return(len(S) - S[::3].count('S') - S[1::3].count('O') - S[2::3].count('S'))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = marsExploration(s)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
